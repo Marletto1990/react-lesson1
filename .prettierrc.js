6eac8f3d083b5943d4d720011629374fea0b4ce6
Evgeny <eialtunin@gmail.com>
2023-12-20 11:29:31 +0300

    chore: prettier, .vscode

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,9 +3,8 @@ module.exports = {
 	trailingComma: 'es5',
 	singleQuote: true,
 	jsxSingleQuote: true,
-	bracketSameLine: true,
 	printWidth: 80,
 	useTabs: true,
 	tabWidth: 2,
-	endOfLine: 'lf',
+	endOfLine: 'crlf',
 };
